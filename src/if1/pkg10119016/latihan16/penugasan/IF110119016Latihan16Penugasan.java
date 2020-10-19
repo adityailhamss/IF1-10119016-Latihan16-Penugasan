3cbfe62e7fb6e72c1d303a87689cd2232a6db0f9
aditi <aditi@DESKTOP-343N0FS>
2020-10-19 22:18:51 +0700

    Upload Project Tugas

---
+++ src/if1/pkg10119016/latihan16/penugasan/IF110119016Latihan16Penugasan.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10119016.latihan16.penugasan;
+
+/**
+ *
+ * @author aditi
+ * NAMA              : Aditya Ilham Subagja
+ * KELAS             : IF1
+ * NIM               : 10119016
+ * Deskripsi Program : Program ini berisi program untuk menampilkan penugasan
+ */
+public class IF110119016Latihan16Penugasan {
+
+    
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int c = 0;
+        
+        System.out.println("Nilai a = " + a);
+        System.out.println("Nilai b = " + b);
+        System.out.println("Nilai c = " + c+"\n");
+        
+        c = a + b;
+        System.out.println("c = a + b = " + c);
+        c += a ;
+        System.out.println("c += a = " + c);
+        c -= a ;
+        System.out.println("c -= a = " + c);
+        c *= a ;
+        System.out.println("c *= a = " + c);
+        a = 10;
+        c = 15;
+        c /= a;
+        System.out.println("c /= a = " + c);
+        a = 10;
+        c = 15;
+        c %= a;
+        System.out.println("c %= a = " + c);
+        c <<= 2;
+        System.out.println("c <<= 2 = " + c);
+        c >>= 2;
+        System.out.println("c >>= 2 = " + c);
+        c >>= 2;
+        System.out.println("c >>= a = " + c);
+        c &= a;
+        System.out.println("c &= 2 = " + c);
+        c ^= a;
+        System.out.println("c ^= a = " + c);
+        c |= a;
+        System.out.println("c |= a = " + c);
+        
+    }
+    
+}
